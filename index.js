e12513c01d87babf2ca29e1fe743e3554d3e70c0
Jonathan <jonathan@xgecko.com>
2016-12-05 10:05:58 -0800

    Adding support for prefixing tag based selectors

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 'use strict';
 var postcss = require('postcss');
 
+var validTags = [
+    'a', 'abbr', 'address', 'area', 'article', 'aside', 'audio', 'b', 'base', 'bdi', 'bdo', 'blockquote',
+    'body', 'br', 'button', 'canvas', 'caption', 'cite', 'code', 'col', 'colgroup', 'data', 'datalist',
+    'dd', 'del', 'dfn', 'div', 'dl', 'dt', 'em', 'embed', 'fieldset', 'figcaption', 'figure', 'footer',
+    'form', 'h1', '-', 'h6', 'head', 'header', 'hr', 'html', 'i', 'iframe', 'img', 'input', 'ins', 'kbd',
+    'keygen', 'label', 'legend', 'li', 'link', 'main', 'map', 'mark', 'meta', 'meter', 'nav', 'noscript',
+    'object', 'ol', 'optgroup', 'option', 'output', 'p', 'param', 'pre', 'progress', 'q', 'rb', 'rp', 'rt',
+    'rtc', 'ruby', 's', 'samp', 'script', 'section', 'select', 'small', 'source', 'span', 'strong', 'style',
+    'sub', 'sup', 'table', 'tbody', 'td', 'template', 'textarea', 'tfoot', 'th', 'thead', 'time', 'title',
+    'tr', 'track', 'u', 'ul', 'var', 'video', 'wbr', '*'
+];
+
 module.exports = postcss.plugin('postcss-class-prefix', classPrefix);
 
 function classPrefix(prefix, options) {
@@ -15,6 +27,15 @@ function classPrefix(prefix, options) {
 
       rule.selectors = rule.selectors.map(function(selector) {
         if (!isClassSelector(selector)) {
+          if (options.tagScope && validTags.filter(function(tag) {
+            //   console.info(`${selector.substr(0, tag.length)} === ${tag}`);
+              return selector.substr(0, tag.length) === tag;
+          }).length > 0) {
+              if (typeof options.tagScope === 'function') {
+                  return options.tagScope(selector);
+              }
+              return options.tagScope + ' ' + selector;
+          }
           return selector;
         }
 
